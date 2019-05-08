b8e95a8a98365158331343e66c3ce03341ca19a7
bcree11 <b_cree@yahoo.com>
2019-05-08 02:00:30 -0700

    new Now site

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,7 +5,7 @@ import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
-import resume from '../resume.pdf'
+import resume from '../static/resume.pdf'
 
 const styles = theme => ({
   root: {
